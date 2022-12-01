e736a24725ccc0de8c7977b47951224eaf2ee328
Noanono <soler.noah@gmail.com>
2022-12-01 21:51:50 +0100

    suite

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ int main(){
     char alphabet_minuscule[26] = {'a','b','c','d','e','f','g','h','i','j',
                                    'k','l','m','n','o','p','q','r','s',
                                    't','u','v','w','x','y','z'};
-
     int i=0;
+    int j=0;
 
     cout << "Entrez le texte à crypter : ";
     cin >> texte;
@@ -22,9 +22,28 @@ int main(){
     cout << "Entrez la clé de cryptage : ";
     cin >> cle;
 
+    int tp_cle;
+    for(int k = 0; k < 100; k++){
+        if(cle[k] == '\0'){
+            tp_cle = k;
+            break;
+        }
+    }
+
     while(texte[i] != '\0'){
-        texte_crypte[i] = ((texte[i] + cle[i]) % 26) + 'A';
+        if(j == tp_cle){
+            j = 0;
+        }
+
+        --cle[j];
+        if(texte[i] >= 'A' && texte[i] <= 'Z'){
+            texte_crypte[i] = alphabet_majuscule[(texte[i] - 'A' + cle[j]) % 26];
+        }else if(texte[i] >= 'a' && texte[i] <= 'z'){
+            texte_crypte[i] = alphabet_minuscule[(texte[i] - 'a' + cle[j]) % 26];
+        }
+
         i++;
+        j++;
     }
     texte_crypte[i] = '\0';
 
