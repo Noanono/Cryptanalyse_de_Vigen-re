35836850c1198d2847ed113682604838fba43219
noah <soler.noah@mail.com>
2022-12-28 19:53:35 +0100

    fonction de cryptage V1 prepa test1

--- main.cpp
+++ main.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 int main(){
 
-    char adresse_decry_file[100];
-    char adresse_cry_file[100];
+    char adresse_decry_file[100] = "./decry_files/Cryptage_test1.txt";
+    char adresse_cry_file[100] = "./cry_files/Cryptage_test1.txt";
     char key[50] = "cochon";
-
-    adresse_decry_file = "./decry_files/Cryptage_test1.txt";
-    adresse_cry_file = "./cry_files/Cryptage_test1.txt";
 
     Crypt_Vigenere(adresse_decry_file, key, adresse_cry_file);
 
